9058aa29efa1f7d0760e05bfebf3791cffdd0dab
alemiranda7 <7alemiranda@gmail.com>
2021-04-05 15:32:44 -0300

    Update desafio001.js

--- scripts/desafio001.js
+++ scripts/desafio001.js
@@ -2,5 +2,5 @@ var vel = 60.4;
 var multa;
 
 if(vel > 60){
-    console.log(`Vc estava dirigindo a ${vel}km/h, sendo assim será <strong>multado</strong>.`)
+    console.log(`Vc estava dirigindo a ${vel}km/h, sendo assim será multado.`)
 }
