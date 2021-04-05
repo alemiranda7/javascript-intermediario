44a2fb0935c3525c6e5f64d8646d7e12977857e7
alemiranda7 <7alemiranda@gmail.com>
2021-04-05 18:16:00 -0300

    alteração e inserção de desafio

--- scripts/desafio001.js
+++ scripts/desafio001.js
@@ -1,6 +1,6 @@
 var vel = 60.4;
 var multa;
 
-if(vel > 60){
+if (vel > 60) {
     console.log(`Vc estava dirigindo a ${vel}km/h, sendo assim será multado.`)
 }
---
+++ scripts/ex011.js
@@ -0,0 +1,10 @@
+var idade = 16;
+console.log(`Sua idade é ${idade}.`)
+
+if (idade < 16) {
+    console.log('Não vota!')
+} else if (idade >= 16 || idade > 65) {
+    console.log('Voto opcional!')
+} else{
+    console.log('Voto obrigatório!')
+}
