35cc6f991fc505827821ae265d70c35a31e45966
alemiranda7 <7alemiranda@gmail.com>
2021-04-12 18:27:04 -0300

    Update script.js

--- contador/script.js
+++ contador/script.js
@@ -1,5 +1,31 @@
-function contar(){
-    var numInicio = document.querySelector('div#n1')
-    var numFim = document.querySelector('div#n2')
-    var numPasso = document.querySelector('div#n3')
+function contar() {
+    var numInicio = document.getElementById('n1')
+    var numFim = document.getElementById('n2')
+    var numPasso = document.getElementById('n3')
+    var resposta = document.getElementById('res')
+
+    if (numInicio.value.length == 0 || numFim.value.length == 0) {
+        alert('Dados Incorretos!')
+    } else {
+        resposta.innerHTML = 'Contando...'
+
+        let i = Number(numInicio.value)
+        let f = Number(numFim.value)
+        let p = Number(numPasso.value)
+        if(p == 0){
+            alert('Passo inválido, será considerado Passo 1')
+            p = 1
+        }
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                resposta.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                resposta.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        resposta.innerHTML += ` \u{1F921}`
+    }
+
 }
