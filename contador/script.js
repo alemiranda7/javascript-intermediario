334de358cfe4efd7d6846dc78554836fdf6fac0b
alemiranda7 <7alemiranda@gmail.com>
2021-04-12 16:41:24 -0300

    Update script.js

--- contador/script.js
+++ contador/script.js
@@ -1,3 +1,5 @@
 function contar(){
-    
+    var numInicio = document.querySelector('div#n1')
+    var numFim = document.querySelector('div#n2')
+    var numPasso = document.querySelector('div#n3')
 }
