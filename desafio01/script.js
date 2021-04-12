a12d93442ae6bfa49c1982716c95e655cb53ae4b
alemiranda7 <7alemiranda@gmail.com>
2021-04-12 15:43:42 -0300

    estruturas de repetição

---
+++ desafio01/script.js
@@ -0,0 +1,16 @@
+let c = 1
+
+console.log('Vai começar o while!')
+while (c <= 5) {
+    console.log('Testando ' + c)
+    c++
+}
+console.log('FIM do while')
+
+console.log('--------------------')
+
+console.log('Começando com o FOR')
+for (let c = 1; c <= 5; c++) {
+    console.log('Testando' + c)
+}
+console.log('Terminando com o For')
