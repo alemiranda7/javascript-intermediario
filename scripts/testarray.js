1e29a0449240c7025792be38da80241348dc70f9
alemiranda7 <7alemiranda@gmail.com>
2021-04-21 11:45:23 -0300

    array test

---
+++ scripts/testarray.js
@@ -0,0 +1,17 @@
+let num = []
+num.push(1)
+num.push(4)
+num.push(2)
+num.push(3)
+num.push(9)
+num.push(5)
+num.push(6)
+num.push(7)
+
+console.log(`O valor do array é ${num}.`)
+console.log(`O tamanho do array é de ${num.length} posições.`)
+
+num.sort()
+
+console.log(`Agora na ordem ${num}.`)
+
---
+++ scripts/testarraytwo.js
@@ -0,0 +1,16 @@
+let num = [5,6,1,4,2,3]
+console.log(num[0])
+console.log(num[1])
+console.log(num[2])
+console.log(num[3])
+console.log(num[4])
+console.log(num[5])
+
+console.log('----------------------------------')
+
+for(let pos = 0;pos<num.length;pos++){
+    //num.sort()
+    console.log(num[pos])
+}
+
+console.log(num.sort())
